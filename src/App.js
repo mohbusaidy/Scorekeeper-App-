00a0a5498ad585a38227901a9f99a37156e1a11b
Mohammed Busaidy <m.busaidy@live.com>
2022-06-30 09:54:42 +0200

    more components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import Player from "./components/Player/Player.js";
+import Button from "./components/Button/Button.js";
+import { initialPreviousGames } from "./historyDB.js";
+
+const players = [
+  {
+    name: "John",
+    score: 15,
+    id: "1",
+  },
+  {
+    name: "Jane",
+    score: 15,
+    id: "2",
+  },
+  {
+    name: "Jim",
+    score: 15,
+    id: "3",
+  },
+];
+
 function App() {
   return (
     <>
-      <h1>Scorekeepr</h1>
+      <heading>
+        <h1>Game</h1>
+      </heading>
+      <main>
+        {players.map((player) => (
+          <Player key={player.id} name={player.name} score={player.score} />
+        ))}
+        <Button background={"red"}>End game</Button>
+      </main>
     </>
   );
 }
